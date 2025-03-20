0f06fdb26b98f0f2de8e495de2ca2fb88bad0cc2
Alexey Melezhik <melezhik@gmail.com>
2025-03-21 01:50:30 +0300

    fix sed

--- tasks/setup-qemu-image/task.bash
+++ tasks/setup-qemu-image/task.bash
@@ -26,9 +26,9 @@ users:
       ssh-rsa %key%
 DATA
 
-KEY=$(cat ~/.ssh/id_rsa.pub)
+export KEY=$(cat ~/.ssh/id_rsa.pub)
 echo $KEY
-sed -i -e "s/%key%/$KEY/" user-data
+sed -i -e 's/%key%/$KEY/' user-data
 
 if test $os = "darwin"; then
   mkisofs -output /tmp/init.iso -volid cidata -joliet -rock user-data meta-data
