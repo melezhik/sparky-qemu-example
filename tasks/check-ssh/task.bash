ee4022d7c78ad0e60ae34b35ad516a1a82e4e9b9
Alexey Melezhik <melezhik@gmail.com>
2025-03-21 01:54:55 +0300

    fix ssh

--- tasks/check-ssh/task.bash
+++ tasks/check-ssh/task.bash
@@ -2,7 +2,7 @@ host=$(config host)
 user=$(config user)
 port=$(config port)
 
-if ssh -o ConnectTimeout=10 -o StrictHostKeyChecking=no -p $port -u $user $host uptime; then
+if ssh -o ConnectTimeout=10 -o StrictHostKeyChecking=no -p $port -l $user $host uptime; then
     echo '{ "state" : "alive" }' > $cache_root_dir/state.json
 else
     echo '{ "state" : "" }' > $cache_root_dir/state.json
