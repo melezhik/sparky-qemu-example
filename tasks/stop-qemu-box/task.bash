6eedf4db639eb9319053546c1fcd66d88f255f6a
Alexey Melezhik <melezhik@gmail.com>
2025-03-20 22:59:37 +0300

    hide errors

--- tasks/stop-qemu-box/task.bash
+++ tasks/stop-qemu-box/task.bash
@@ -1,5 +1,3 @@
-set -x
-
-ps axf | grep "qemu-system-aarch64" | grep -v grep | awk '{print "kill -9 " $1}' | sh
+ps axf | grep "qemu-system-aarch64" | grep -v grep | awk '{print "kill -9 " $1}' | sh 2>/dev/null
 
 echo "done"
