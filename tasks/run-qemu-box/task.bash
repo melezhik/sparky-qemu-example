93fbca940dab2bbf40face1a5eb755f7ddcbaa2b
Alexey Melezhik <melezhik@gmail.com>
2025-03-31 23:23:51 +0300

    trytry to address CPU does not support x86-64-v2  issue

--- tasks/run-qemu-box/task.bash
+++ tasks/run-qemu-box/task.bash
@@ -27,6 +27,7 @@ else
     -net nic \
     -net user,hostfwd=tcp::10022-:22 \
     -m 6024M \
+    -cpu max \
     -smp 8 \
     -vnc none \
     -drive "file=$iso,index=0,format=qcow2,media=disk" \
