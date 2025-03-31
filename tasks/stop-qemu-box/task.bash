88c0e89fa000729b1068052293008931108fa8e7
Alexey Melezhik <melezhik@gmail.com>
2025-03-31 23:00:11 +0300

    fix typo

--- tasks/stop-qemu-box/task.bash
+++ tasks/stop-qemu-box/task.bash
@@ -1,6 +1,8 @@
 qemu_binary=$(config qemu_binary)
 
-ps aux | grep "${qemu_binary}" | grep -v grep | awk '{print "kill -9 " $2}' | sh
+echo "killing by $qemu_binary"
+
+ps aux | grep "$qemu_binary" | grep -v grep | awk '{print "kill -9 " $2}' | sh
 #ps aux | grep "${qemu_binary}" | grep -v grep | awk '{print "kill -9 " $2}'
 
 echo "done"
